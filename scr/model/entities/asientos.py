0f3fbe6efc202cca68eec9ab0862f078178f752a
shura072717 <luisgonzalo.5g@gmail.com>
2026-04-05 08:06:53 -0500

    asientos

---
+++ scr/model/entities/asientos.py
@@ -0,0 +1,7 @@
+class Asiento:
+    def __init__(self, id, avion_id, numero_asiento, clase, estado):
+        self.id = id
+        self.avion_id = avion_id
+        self.numero_asiento = numero_asiento
+        self.clase = clase
+        self.estado = estado
